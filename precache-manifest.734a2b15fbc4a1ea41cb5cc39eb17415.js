7981c1413a21d181acedaeb72b093acc16d46e3c
Helena2018 <helena2020liu@gmail.com>
2022-06-17 13:28:05 +1200

    Updates

---
+++ precache-manifest.734a2b15fbc4a1ea41cb5cc39eb17415.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "99fa9fbb47656b0147ac",
+    "url": "/robofriends/static/css/main.fc7ea27a.chunk.css"
+  },
+  {
+    "revision": "99fa9fbb47656b0147ac",
+    "url": "/robofriends/static/js/main.0eb1d745.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "4bd876e8a51a123f608b",
+    "url": "/robofriends/static/css/2.23026db5.chunk.css"
+  },
+  {
+    "revision": "4bd876e8a51a123f608b",
+    "url": "/robofriends/static/js/2.cf92c1d9.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e8349d985ed3612090443019d3a510f2",
+    "url": "/robofriends/index.html"
+  }
+];
